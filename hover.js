2db72a5afb8e5faf18d385a7d9880cb4038e6444
David Montoya Ramírez <deividelpaisa@hotmail.com>
2021-05-17 02:40:05 -0500

    Sección datos terminada

---
+++ hover.js
@@ -0,0 +1,18 @@
+/* let seccionDatos = document.getElementById("datos")
+let idex19 = document.getElementById("index-19")
+
+
+
+seccionDatos.addEventListener("mouseover", hoverSectionOver)
+
+seccionDatos.addEventListener("mouseout", hoverSectionOut)
+
+function hoverSectionOut() {
+    idex19.classList.remove("index-19") 
+}
+
+
+function hoverSectionOver() {
+     idex19.classList.add("index-19") 
+}
+ */
